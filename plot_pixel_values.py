21729176029143605a015af757ff5086f4524950
Jean Connelly <jconnelly@cfa.harvard.edu>
2015-09-03 16:06:18 -0400

    Add fitting and optional plotting of fits

--- plot_pixel_values.py
+++ plot_pixel_values.py
@@ -3,10 +3,12 @@
 from time import sleep
 import argparse
 
+import logging
 from scipy.ndimage.filters import median_filter
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.table import Table
+from sherpa import ui
 from Chandra.Time import DateTime
 
 
@@ -22,7 +24,8 @@ def get_opt():
     parser.add_argument('--fit-scaling',
                         action='store_true',
                         help='Fit the dark and scale factor (default=False)')
-
+    parser.add_argument('--plot-fits',
+                        action='store_true')
     parser.add_argument('--n-brightest',
                         default=64,
                         type=int,
@@ -32,6 +35,51 @@ def get_opt():
     return args
 
 opt = get_opt()
+
+
+def scale_model(pars, t_ccd):
+    scale, y_const, x_const = pars
+    return 1 / np.exp(np.log(scale) / 4.0 * (t_ccd - x_const))
+
+
+def fit_pix_values(t_ccd, esec):
+    logger = logging.getLogger("sherpa")
+    logger.setLevel(logging.WARN)
+    data_id = 1
+    ui.set_method('simplex')
+    ui.set_stat('cash')
+    ui.load_user_model(scale_model, 'model')
+    ui.add_user_pars('model', ['scale', 'y_const', 'x_const'])
+    ui.set_model(data_id, 'model')
+    ui.load_arrays(data_id,
+                   np.array(t_ccd),
+                   np.array(esec)
+                   )
+    model.scale.val = 0.70
+    ui.fit(data_id)
+    return ui.get_fit_results()
+
+
+def print_info_block(fits, dat):
+    print("*************************************************")
+    print("Time = {}".format(DateTime(dat[-1]['time']).date))
+    print("CCD temperature = {}".format(dat[-1]['TEMPCD']))
+    print("Slot = {}\n".format(dat[-1]['SLOT']))
+    print("Fit values:\n")
+    mini_table = []
+    for pix_id in sorted(fits):
+        fit = fits[pix_id]
+        t_sf = scale_model(fit.parvals, dat[-1]['TEMPCD'])
+        m_sf = scale_model(fit.parvals, -19)
+        minus_19_val = dat[-1][pix_id] * m_sf / t_sf
+        mini_table.append([pix_id, dat[-1][pix_id], minus_19_val, fit.parvals[0]])
+    mini_table = Table(rows=mini_table,
+                       names=['PixId', 'Val', 'Val(-19)', 'Scale'])
+    mini_table['Val(-19)'].format='.2f'
+    mini_table['Scale'].format='.4f'
+    print mini_table
+    print "*************************************************"
+
 
 plt.close(1)
 plt.close(2)
@@ -81,6 +129,7 @@ while True:
         fig = figs[im]
         axes = axess[im]
         i_col = 0
+        fits = {}
         for r in range(N):
             for c in range(N):
                 ax = axes[r][c]
@@ -98,7 +147,16 @@ while True:
                             xy=(0.5, 0.5), xycoords="axes fraction",
                             ha='center', va='center',
                             color='lightgrey')
+                if opt.fit_scaling:
+                    nonzero = y != 0
+                    fit = fit_pix_values(dat['TEMPCD'][nonzero],
+                                         y[nonzero])
+                    fits[y.name] = fit
+                    if opt.plot_fits:
+                        plt.figure("fitfig_{}".format(y.name))
+                        ui.plot_fit(replot=True, overplot=True)
 
+        print_info_block(fits, dat)
         plt.draw()
         fig.canvas.draw()
         fig.canvas.flush_events()
